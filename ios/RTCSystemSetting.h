f5f19ad265fc9fa2baa47e30b005a65fe3053ded
Alexandru-Cristian Belu <alexandru.belu@softstatetechnologies.com>
2021-08-12 13:16:35 +0300

    Imported header to library

--- ios/RTCSystemSetting.h
+++ ios/RTCSystemSetting.h
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
+#import <React/RCTLog.h>
 
 @interface RCTSystemSetting : RCTEventEmitter <RCTBridgeModule>
 
