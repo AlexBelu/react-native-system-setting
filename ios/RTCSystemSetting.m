cb256a91d09db38b97cf48cafa7ba6c43092423b
Alexandru-Cristian Belu <alexandru.belu@softstatetechnologies.com>
2021-08-18 14:21:59 +0300

    sendEvent when changing brightness

--- ios/RTCSystemSetting.m
+++ ios/RTCSystemSetting.m
@@ -82,6 +82,7 @@ RCT_EXPORT_METHOD(setBrightness:(float)val resolve:(RCTPromiseResolveBlock)resol
         [[UIScreen mainScreen] setBrightness:val];
         resolve([NSNumber numberWithBool:YES]);
     });
+    [self sendEventWithName:@"BrightnessLevelDidChange" body:@(val)];
 }
 
 RCT_EXPORT_METHOD(getBrightness:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
